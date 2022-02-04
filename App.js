47534a984f0b1b237d3100f7cf673ed7c0fa822c
Nicolas Posa <nicoposa57@gmail.com>
2022-02-04 18:22:19 -0300

    adding the project

--- App.js
+++ App.js
@@ -1,20 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, Text, StyleSheet, View, FlatList, TextInput } from 'react-native';
+// import { SearchBar } from 'react-native-elements';
 
-export default function App() {
+
+const App = () => {
+  const [search, setSearch] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setFilteredData(responseJson);
+        setData(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const searchFilter = (text) => {
+    if (text) {
+      const newData = data.filter(function (item) {
+        const itemData = item.title
+          ? item.title.toUpperCase()
+          : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredData(newData);
+      setSearch(text);
+    } else {
+      setFilteredData(data);
+      setSearch(text);
+    }
+  };
+
+  const ItemView = ({ item }) => {
+    return (
+      <Text style={styles.itemStyle}>
+        {item.id}
+        {'.'}
+        {item.title.toUpperCase()}
+      </Text>
+    );
+  };
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+      <TextInput
+          onChangeText={(text) => searchFilter(text)}
+          placeholder="Type Here..."
+          value={search}
+          style={styles.searchInput}
+        />
+        {/* <SearchBar */}
+        {/* //   onChangeText={(text) => searchFilter(text)}
+        //   placeholder="Type Here..."
+        //   value={search}
+        //   style={styles.searchInput}
+        //   round
+        //   searchIcon={{ size: 24 }}
+        //   onClear={(text) => searchFilter('')}
+        // /> */}
+        <FlatList
+          data={filteredData}
+          keyExtractor={item => item.id}
+          renderItem={ItemView}
+        />
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    backgroundColor: 'lightblue',
+  },
+  itemStyle: {
+    padding: 10,
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderRadius: 15,
+    margin: 5,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  searchInput: {
+    height: 50,
+    borderColor: '#0f0f0f',
+    borderWidth: 1,
+    margin: 10,
+    padding: 10,
+    borderRadius: 15,
+    backgroundColor: '#fff',
   },
 });
+
+export default App;
